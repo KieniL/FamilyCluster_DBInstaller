57b81790c337f55e2c6b6df22d1e59c13f66ce2a
Lukas Kienast <lukaskienast0@gmail.com>
2021-06-26 06:27:45 +0200

    corrected entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-sed -i 's|$AUTH_DB|'"$AUTH_DB"'|g' init.sql
-sed -i 's|$CERT_DB|'"$CERT_DB"'|g' init.sql
-sed -i 's|$ANSPAR_DB|'"$ANSPAR_DB"'|g' init.sql
-sed -i 's|$AUTH_USER|'"$AUTH_USER"'|g' init.sql
-sed -i 's|$AUTH_PASS|'"$AUTH_PASS"'|g' init.sql
-sed -i 's|$CERT_USER|'"$CERT_USER"'|g' init.sql
-sed -i 's|$CERT_PASS|'"$CERT_PASS"'|g' init.sql
-sed -i 's|$ANSPAR_USER|'"$ANSPAR_USER"'|g' init.sql
-sed -i 's|$ANSPAR_PASS|'"$ANSPAR_PASS"'|g' init.sql
+export SQL_FILE="init.sql"
+export TMP_FILE="/tmp/init.sql"
+
+cp $SQL_FILE $TMP_FILE
+
+sed -i "s/\$AUTH_DB/$AUTH_DB/" $TMP_FILE
+sed -i "s/\$CERT_DB/$CERT_DB/" $TMP_FILE
+sed -i "s/\$ANSPAR_DB/$ANSPAR_DB/" $TMP_FILE
+sed -i "s/\$AUTH_USER/$AUTH_USER/" $TMP_FILE
+sed -i "s/\$AUTH_PASS/$AUTH_PASS/" $TMP_FILE
+sed -i "s/\$CERT_USER/$CERT_USER/" $TMP_FILE
+sed -i "s/\$CERT_PASS/$CERT_PASS/" $TMP_FILE
+sed -i "s/\$ANSPAR_USER/$ANSPAR_USER/" $TMP_FILE
+sed -i "s/\$ANSPAR_PASS/$ANSPAR_PASS/" $TMP_FILE
+
+cat $TMP_FILE > $SQL_FILE
 
 mysql --host=$DB_HOST --port=$DB_PORT --user=$DB_ADMIN_USER --password=$DB_ADMIN_PASSWORD < init.sql

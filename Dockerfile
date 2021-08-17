c18610987d28581e4782fcefee6360faef34fd0b
Lukas Kienast <lukaskienast0@gmail.com>
2021-08-17 10:27:27 +0200

    use file instead of env var

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,15 @@
-FROM luke19/mysql-base-image:1627573675
+FROM luke19/mysql-base-image:1629007085
 
-ENV DB_HOST=tmp
-ENV DB_PORT=1
-ENV DB_ADMIN_USER=tmp
-ENV DB_ADMIN_PASSWORD=tmp
-ENV AUTH_DB=tmp
-ENV AUTH_USER=tmp
-ENV AUTH_PASS=tmp
-ENV ANSPAR_DB=tmp
-ENV ANSPAR_USER=tmp
-ENV ANSPAR_PASS=tmp
-ENV CERT_DB=tmp
-ENV CERT_USER=tmp
-ENV CERT_PASS=tmp
 ENV ADD_DUMMYDATA=false
+
+VOLUME /etc/config
 
 WORKDIR /install
 
 
 COPY entrypoint.sh /install/entrypoint.sh
-COPY --chown=dbuser init.sql /install/init.sql
-COPY --chown=dbuser dummydata.sql /install/dummydata.sql
+COPY --chown=mysql init.sql /install/init.sql
+COPY --chown=mysql dummydata.sql /install/dummydata.sql
 
 RUN chmod 777 init.sql && chmod 777 dummydata.sql
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+
+# Read the content of the secret into variables for usage
+# Run this in the shell inside the container
+AUTH_DB=$(</etc/config/AUTH_DB)
+CERT_DB=$(</etc/config/CERT_DB)
+ANSPAR_DB=$(</etc/config/ANSPAR_DB)
+AUTH_USER=$(</etc/config/AUTH_USER)
+AUTH_PASS=$(</etc/config/AUTH_PASS)
+CERT_USER=$(</etc/config/CERT_USER)
+CERT_PASS=$(</etc/config/CERT_PASS)
+ANSPAR_USER=$(</etc/config/ANSPAR_USER)
+ANSPAR_PASS=$(</etc/config/ANSPAR_PASS)
+DB_HOST=$(</etc/config/DB_HOST)
+DB_PORT=$(</etc/config/DB_PORT)
+DB_ADMIN_USER=$(</etc/config/DB_ADMIN_USER)
+DB_ADMIN_PASSWORD=$(</etc/config/DB_ADMIN_PASSWORD)
+
 export SQL_INIT_FILE="init.sql"
 export TMP_INIT_FILE="/tmp/init.sql"
 export SQL_DUMMY_FILE="dummydata.sql"
